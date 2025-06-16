2525880f1a60b358902f38f41d417f6fc83df99a
Maks Rafalko <maks.rafalko@gmail.com>
2025-06-16 22:30:21 +0200

    Initial commit

---
+++ src/SomeClass.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaksRafalko\RandomPhpTests;
+
+final class SomeClass
+{
+    public function test(): int
+    {
+        return 2; // to trigger tests-checker2
+    }
+}
